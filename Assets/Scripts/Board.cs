ba0628d7ed314d92b2992f7e4e99a967402f2a47
jhonzasd <jhonjairotejada@gmail.com>
2023-12-26 18:59:15 -0500

    Añade eliminación de matches por defecto

--- Assets/Scripts/Board.cs
+++ Assets/Scripts/Board.cs
@@ -1,5 +1,7 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 using UnityEngine.UIElements;
 
@@ -19,6 +21,8 @@ public class Board : MonoBehaviour
 
     private Tile startTile;
     private Tile endTile;
+
+    private bool swappingPieces = false;
     
     // Start is called before the first frame update
     void Start()
@@ -33,20 +37,51 @@ public class Board : MonoBehaviour
 
     private void SetupPieces()
     {
+        int maxIterations = 50;
+        int currentIteration = 0;
         for (int x = 0; x < width; x++)
         {
             for (int y = 0; y < height; y++)
             {
-                
-                var selectedPiece = availablePieces[UnityEngine.Random.Range(0, availablePieces.Length)];
-                var o = Instantiate(selectedPiece, new Vector3(x, y, 1), Quaternion.identity);
-                o.transform.parent = transform;
-                Pieces[x, y] = o.GetComponent<Piece>();
-                Pieces[x,y].Setup(x, y, this);
-                
-                
+                currentIteration = 0;
+
+                var newPiece = CreatePieceAt(x, y);
+                while (HasPreviousMatches(x, y))
+                {
+                    ClearPieceAt(x, y);
+                    newPiece = CreatePieceAt(x, y);
+                    currentIteration++;
+                    if (currentIteration > maxIterations)
+                    {
+                        break;
+                    }
+                    
+                }
+
+
             }
         }
+
+        
+    }
+
+    private void ClearPieceAt(int x, int y)
+    {
+        var pieceToClear = Pieces[x, y];
+        Destroy(pieceToClear.gameObject);
+        pieceToClear = null;
+        
+    }
+
+    private Piece CreatePieceAt(int x, int y)
+    {
+        var selectedPiece = availablePieces[UnityEngine.Random.Range(0, availablePieces.Length)];
+        var o = Instantiate(selectedPiece, new Vector3(x, y, 1), Quaternion.identity);
+        o.transform.parent = transform;
+        Pieces[x, y] = o.GetComponent<Piece>();
+        Pieces[x,y].Setup(x, y, this);
+        
+        return Pieces[x,y];
     }
 
     private void PositionCamera()
@@ -87,16 +122,15 @@ public class Board : MonoBehaviour
     }
     public void TileUp(Tile tile_)
     {
-        if (startTile!=null && endTile != null)
+        if (startTile!=null && endTile != null && IsCloseTo(startTile, endTile))
         {
-            SwapTiles();
+            StartCoroutine(SwapTiles());
         }
 
-        startTile = null;
-        endTile = null;
+        
     }
 
-    private void SwapTiles()
+    private IEnumerator SwapTiles()
     {
         var StartPiece = Pieces[startTile.x, startTile.y]; // Se obtiene la posición de la pieza que estaba en el espacio incial
         var EndPiece = Pieces[endTile.x, endTile.y]; // Se obtiene la posición de la pieza que estaba en el espacio final
@@ -106,5 +140,144 @@ public class Board : MonoBehaviour
 
         Pieces[startTile.x, startTile.y] = EndPiece;
         Pieces[endTile.x, endTile.y] = StartPiece;
+
+        yield return new WaitForSeconds(0.6f);
+
+        bool foundMatch = false;
+        var startMatches = GetMatchByPiece(startTile.x, startTile.y, 3); // Busca los matches de la pieza inicial
+        var endMatches = GetMatchByPiece(endTile.x, endTile.y, 3); // Busca los matches de la pieza final
+        
+        startMatches.ForEach(piece =>
+        {
+            foundMatch = true;
+            Pieces[piece.x, piece.y] = null;
+            Destroy(piece.gameObject);
+        });
+        
+        endMatches.ForEach(piece =>
+        {
+            foundMatch = true;
+            Pieces[piece.x, piece.y] = null;
+            Destroy(piece.gameObject);
+        });
+
+        if (!foundMatch)
+        {
+            StartPiece.Move(startTile.x, startTile.y);
+            EndPiece.Move(endTile.x, endTile.y);
+            Pieces[startTile.x, startTile.y] = StartPiece;
+            Pieces[endTile.x, endTile.y] = EndPiece;
+        }
+        
+        startTile = null;
+        endTile = null;
+        swappingPieces = false;
+
+        yield return null;
+
     }
+
+    public bool IsCloseTo(Tile start, Tile end)
+    {
+        if(Math.Abs((start.x-end.x))==1 && start.y == end.y)
+        {
+            return true;
+        }
+
+        if (Math.Abs(start.y - end.y) == 1 && start.x == end.x)
+        {
+            return true;
+        }
+
+        return false;
+    }
+
+    bool HasPreviousMatches(int posx, int posy)
+    {
+        var downMatches = GetMatchByDirection(posx, posy, new Vector2(0, -1), 2);
+        var leftMatches = GetMatchByDirection(posx, posy, new Vector2(-1, 0), 2);
+
+        if (downMatches == null) downMatches = new List<Piece>();
+        if (leftMatches == null) leftMatches = new List<Piece>();
+        return (downMatches.Count > 0 || leftMatches.Count > 0);
+    }   
+    
+
+    public List<Piece> GetMatchByDirection(int xpos, int ypos, Vector2 direction, int minPieces = 3)
+    {
+        List<Piece> matches = new List<Piece>();
+        Piece startPiece = Pieces[xpos, ypos];
+        matches.Add(startPiece);
+
+        int nextX;
+        int nextY;
+        int maxVal = width > height ? width : height;
+
+        for (int i = 1; i < maxVal; i++)
+        {
+            nextX = xpos + ((int)direction.x * i);
+            nextY = ypos + ((int)direction.y * i);
+
+            if (nextX >= 0 && nextX < width && nextY >= 0 && nextY < height)
+            {
+                var nextPiece = Pieces[nextX, nextY];
+                if (nextPiece != null && nextPiece.pieceType == startPiece.pieceType)
+                {
+                    matches.Add(nextPiece);
+                }
+                else
+                {
+                    break;
+                }
+
+            }
+        }
+
+        if (matches.Count >= minPieces)
+        {
+            return matches;
+        }
+
+        return null;
+    }
+
+    public List<Piece> GetMatchByPiece(int xpos, int ypos, int minPieces = 3)
+    {
+        var upMatchs = GetMatchByDirection(xpos, ypos, 
+            new Vector2(0, 1), 2);
+        var downMatchs = GetMatchByDirection(xpos, ypos, 
+            new Vector2(0, -1), 2);
+        var rightMatchs = GetMatchByDirection(xpos, ypos, 
+            new Vector2(1, 0), 2);
+        var leftMatchs = GetMatchByDirection(xpos, ypos, 
+            new Vector2(-1, 0), 2);
+
+        if (upMatchs == null) upMatchs = new List<Piece>();
+        if (downMatchs == null) downMatchs = new List<Piece>();
+        if (rightMatchs == null) rightMatchs = new List<Piece>();
+        if (leftMatchs == null) leftMatchs = new List<Piece>();
+
+        var verticalMatches = upMatchs.Union(downMatchs).ToList();
+        var horizontalMatches = leftMatchs.Union(rightMatchs).ToList();
+
+        var foundMatches = new List<Piece>();
+
+        if (verticalMatches.Count >= minPieces)
+        {
+            foundMatches = foundMatches.Union(verticalMatches).ToList();
+        }
+
+        if (horizontalMatches.Count >= minPieces)
+        {
+            foundMatches = foundMatches.Union(horizontalMatches).ToList();
+        }
+
+        return foundMatches;
+
+
+    }
+    
+    
+        
+    
 }
